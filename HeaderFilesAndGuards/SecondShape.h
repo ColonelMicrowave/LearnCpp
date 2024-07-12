5b4ea199b4f50c2f7e9ca5a216bb3fa70953b6e5
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-07-12 22:27:36 +0100

    Create SecondShape.h

---
+++ HeaderFilesAndGuards/SecondShape.h
@@ -0,0 +1,10 @@
+// Check if SECOND_SHAPE_H has not been defined
+#ifndef SECOND_SHAPE_H
+// If not defined, define SECOND_SHAPE_H
+#define SECOND_SHAPE_H
+
+// Forward declaration of function getNumOfSides
+int getNumOfSides();
+
+#endif // End of SECOND_SHAPE_H
+// End of SecondShape.h
