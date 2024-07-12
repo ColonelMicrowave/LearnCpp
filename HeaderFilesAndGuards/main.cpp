04fdcbe95cdc813883d6b74db17fedb5bfbee7ac
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-07-12 22:24:07 +0100

    Create main.cpp

---
+++ HeaderFilesAndGuards/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>      // Needed for std::cout
+#include "Shape.h"       // Include the header file Shape.h
+#include "SecondShape.h" // Include the header file SecondShape.h
+
+// Define the number of sides of a shape
+int main()
+{
+	std::cout << getNumOfSides() << std::endl;
+	return 0;
+}
+// End of main.cpp
