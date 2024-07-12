3ab151dec7f894f57f79507892b6b8e0873c5bf8
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-07-12 22:50:02 +0100

    Update and rename SecondShape.hpp to SecondShape.h

---
+++ HeaderFilesAndGuards/SecondShape.h
@@ -0,0 +1,9 @@
+#ifndef SECOND_SHAPE_H
+#define SECOND_SHAPE_H
+
+// Include the Shape.h file
+#include "Shape.h"
+
+
+#endif // End of SECOND_SHAPE_H
+// End of SecondShape.h
--- HeaderFilesAndGuards/SecondShape.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SECOND_SHAPE_HPP
-#define SECOND_SHAPE_HPP
-
-// Include the Shape.hpp file
-#include "Shape.hpp"
-
-
-#endif // End of SECOND_SHAPE_HPP
-// End of SecondShape.hpp
