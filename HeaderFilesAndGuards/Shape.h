89b58e76d93e7db00a3bcd4b5b469bcb6f41a690
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-07-12 22:44:56 +0100

    Update Shape.h

--- HeaderFilesAndGuards/Shape.h
+++ HeaderFilesAndGuards/Shape.h
@@ -1,10 +1,11 @@
-// Check if SHAPE_H is not defined
-#ifndef SHAPE_H
-// If not defined, define SHAPE_H
-#define SHAPE_H
+#ifndef SHAPE_H // Check if SHAPE_H is not defined
+#define SHAPE_H //If not defined, define SHAPE_H
 
-// Forward declaration of function getNumOfSides
-int getNumOfSides();
+// Defining function getNumOfSides
+int getNumOfSides()
+{
+	return 4;
+}
 
 #endif // End of SHAPE_H
 // End of Shape.h
