537aa47db7478a4129a23f9ddb06184c420489a7
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-09-02 13:44:31 +0100

    Create UsingStrings.cpp

---
+++ UsingStrings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Enter your full name: ";
+	std::string name{ };
+	std::getline(std::cin >> std::ws, name);
+	int nameLength{ static_cast <int>(name.length()) }; // static_cast <int> used to convert the result of name.length() to a signed integer so we do not mix signed and unsigned integers
+
+	std::cout << "Enter your age: ";
+	int age{ }; // age is a signed integer so we can add it to nameLength
+	std::cin >> age;
+
+	std::cout << "Your age + length of name is: " << age + nameLength << "\n";
+	return 0;
+}
