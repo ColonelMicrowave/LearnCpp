00380b5bc00f6590c1a31acfa3ad0847d4c6b778
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-09-02 13:48:20 +0100

    Update UsingStrings.cpp

--- UsingStrings.cpp
+++ UsingStrings.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	std::cout << "Enter your full name: ";
 	std::string name{ };
-	std::getline(std::cin >> std::ws, name);
+	std::getline(std::cin >> std::ws, name); // std::ws used so std::cin will ignore leading whitespace and getline will read the entire line including spaces
 	int nameLength{ static_cast <int>(name.length()) }; // static_cast <int> used to convert the result of name.length() to a signed integer so we do not mix signed and unsigned integers
 
 	std::cout << "Enter your age: ";
