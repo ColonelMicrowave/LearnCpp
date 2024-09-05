6a470502ef9069b166cdb8e6c8e53c452d14fe9d
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-09-05 20:50:34 +0100

    Create BitwiseRotationSimple.cpp

---
+++ BitManipulation/BitwiseRotationSimple.cpp
@@ -0,0 +1,25 @@
+#include <bitset>
+#include <iostream>
+
+std::bitset<4> rotateLeft(std::bitset<4> bits)
+{
+	bool bitCheck = bits.test(3);
+
+	bits <<= 1;
+
+	if (bitCheck != bits.test(3))
+		bits.set(0);
+
+	return bits;
+}
+
+int main()
+{
+	std::bitset<4> bits1{ 0b0001 };
+	std::cout << rotateLeft(bits1) << '\n';
+
+	std::bitset<4> bits2{ 0b1001 };
+	std::cout << rotateLeft(bits2) << '\n';
+
+	return 0;
+}
