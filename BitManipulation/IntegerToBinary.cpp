9fa5b39d35a111a04259b249b93f737becb81ea1
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-09-06 12:54:16 +0100

    Update IntegerToBinary.cpp

---
+++ BitManipulation/IntegerToBinary.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void printBit(int userNum, int binaryDivisor)
+{
+    std::cout << ((userNum / binaryDivisor) % 2);
+}
+
+void print8BitBinary (int userNum)
+{
+    printBit(userNum, 128);
+    printBit(userNum, 64);
+    printBit(userNum, 32);
+    printBit(userNum, 16);
+    
+    std::cout << " ";
+    
+    printBit(userNum, 8);
+    printBit(userNum, 4);
+    printBit(userNum, 2);
+    printBit(userNum, 1);
+}
+
+int main()
+{
+    std::cout << "Enter an integer between 0 and 255: ";
+    int userNum{ };
+    std::cin >> userNum;
+    
+    print8BitBinary(userNum);
+    
+    return 0;
+}
