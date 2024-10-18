39d75de4e91178df0f965b1b63a064d8175bcd5b
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-10-18 20:22:07 +0100

    Create random.h

---
+++ ObjectedOrientedProgramming/BasicOOP/RandomMonsterGenerator/random.h
@@ -0,0 +1,42 @@
+// provided by learncpp.com
+
+#ifndef RANDOM_MT_H
+#define RANDOM_MT_H
+
+#include <chrono>
+#include <random>
+
+namespace Random
+{
+	inline std::mt19937 generate()
+	{
+		std::random_device rd{};
+
+		std::seed_seq ss{
+			static_cast<std::seed_seq::result_type>(std::chrono::steady_clock::now().time_since_epoch().count()),
+				rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+
+		return std::mt19937{ ss };
+	}
+
+	inline std::mt19937 mt{ generate() };
+
+	inline int get(int min, int max)
+	{
+		return std::uniform_int_distribution{ min, max }(mt);
+	}
+
+	template <typename T>
+	T get(T min, T max)
+	{
+		return std::uniform_int_distribution<T>{min, max}(mt);
+	}
+
+	template <typename R, typename S, typename T>
+	R get(S min, T max)
+	{
+		return get<R>(static_cast<R>(min), static_cast<R>(max));
+	}
+}
+
+#endif
