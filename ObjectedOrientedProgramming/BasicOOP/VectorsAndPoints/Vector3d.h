0695f5d68e1cbd0679616eb1eeb9239bbaa3f7e9
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-10-18 08:04:58 +0100

    Create Vector3d.h

---
+++ ObjectedOrientedProgramming/BasicOOP/VectorsAndPoints/Vector3d.h
@@ -0,0 +1,21 @@
+#ifndef VECTOR3D_H
+#define VECTOR3D_H
+
+#include "Point3d.h"
+
+class Vector3d
+{
+private:
+	double m_x{};
+	double m_y{};
+	double m_z{};
+
+public:
+	Vector3d(double x, double y, double z);
+
+	void print() const;
+
+	friend void Point3d::moveByVector(const Vector3d& v);
+};
+
+#endif
