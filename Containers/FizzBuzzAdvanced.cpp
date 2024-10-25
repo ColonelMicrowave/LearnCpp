66a1f55367bf3dddbf49824b3551288c68b092ac
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2024-10-25 15:19:26 +0100

    Create FizzBuzzAdvanced.cpp

---
+++ Containers/FizzBuzzAdvanced.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string_view>
+#include <cassert>
+
+int main()
+{
+	std::vector<std::string_view> fizzbuzz{ "fizz", "buzz", "pop", "bang", "jazz", "pow", "boom" };
+	std::vector<int> divisors{ 3, 5, 7, 11, 13, 17, 19 };
+
+	assert((fizzbuzz.size() == divisors.size()) && "FizzBuzz and Divisors size mismatch");
+
+	for (int i{ 1 }; i <= 150; ++i)
+	{
+		bool isDivisible{ false };
+		for (std::size_t j{ 0 }; j < divisors.size(); ++j)
+		{
+			if (i % divisors[j] == 0)
+			{
+				std::cout << fizzbuzz[j];
+				isDivisible = true;
+			}
+		}
+
+		if (!isDivisible)
+			std::cout << i;
+		std::cout << '\n';
+	}
+}
