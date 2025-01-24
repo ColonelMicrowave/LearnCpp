0457fdbb6a812186a948f0cb4bc11220fa5241a8
ColonelMicrowave <107807539+ColonelMicrowave@users.noreply.github.com>
2025-01-24 19:12:30 +0000

    Implemented function pointers

--- SortingAlgorithms/SimpleSorter.cpp
+++ SortingAlgorithms/SimpleSorter.cpp
@@ -2,31 +2,82 @@
 #include <iterator>
 #include <utility>
 
-int main()
+bool descending(int x, int y)
 {
-	int array[]{ 30, 50, 20, 10, 40 };
-	constexpr int length{ static_cast<int>(std::size(array)) };
+	return x < y;
+}
 
+bool ascending(int x, int y)
+{
+	return x > y;
+}
+
+bool oddsFirst(int x, int y)
+{
+	if ((x % 2 == 0) && !(y % 2 == 0))
+		return true;
+	else if (!(x % 2 == 0) && (y % 2 == 0))
+		return false;
+	else
+		return ascending(x, y);
+}
+
+bool evensFirst(int x, int y)
+{
+	if ((x % 2 == 0) && !(y % 2 == 0))
+		return false;
+	else if (!(x % 2 == 0) && (y % 2 == 0))
+		return true;
+	else
+		return ascending(x, y);
+}
+
+void selectionSort(int* array, int length, bool (*comparisonFnc)(int, int) = ascending)
+{
 	for (int startIndex{ 0 }; startIndex < length - 1; ++startIndex)
 	{
-		int largestIndex{ startIndex };
+		int bestIndex{ startIndex };
 
 		for (int currentIndex{ startIndex + 1 }; currentIndex < length; ++currentIndex)
 		{
-			if (array[largestIndex] < array[currentIndex])
+			if (comparisonFnc(array[bestIndex], array[currentIndex]))
 			{
-				largestIndex = currentIndex;
+				bestIndex = currentIndex;
 			}
 		}
 
-		std::swap(array[startIndex], array[largestIndex]);
+		std::swap(array[startIndex], array[bestIndex]);
 	}
+}
+
+void printArray(int* array, int length)
+{
+	if (!array)
+		return;
 
 	for (int i{ 0 }; i < length; ++i)
 	{
 		std::cout << array[i] << ' ';
 	}
 	std::cout << '\n';
+}
+
+int main()
+{
+	int array[]{ 3, 5, 2, 1, 4 };
+	constexpr int length{ static_cast<int>(std::size(array)) };
+
+	selectionSort(array, length);
+	printArray(array, length);
+
+	selectionSort(array, length, descending);
+	printArray(array, length);
+
+	selectionSort(array, length, evensFirst);
+	printArray(array, length);
+
+	selectionSort(array, length, oddsFirst);
+	printArray(array, length);
 
 	return 0;
 }
